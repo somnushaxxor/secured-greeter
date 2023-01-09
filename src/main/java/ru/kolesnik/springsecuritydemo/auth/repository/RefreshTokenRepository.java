1a947340ef8d6698b08fc1f8cbc9cc14547fd747
Artem Kolesnik <steeltouch27@gmail.com>
2023-01-09 20:47:28 +0600

    Add @Repository annotation

--- src/main/java/ru/kolesnik/springsecuritydemo/auth/repository/RefreshTokenRepository.java
+++ src/main/java/ru/kolesnik/springsecuritydemo/auth/repository/RefreshTokenRepository.java
@@ -1,10 +1,12 @@
 package ru.kolesnik.springsecuritydemo.auth.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import ru.kolesnik.springsecuritydemo.auth.model.RefreshToken;
 
 import java.util.Optional;
 
+@Repository
 public interface RefreshTokenRepository extends JpaRepository<RefreshToken, Long> {
 
     Optional<RefreshToken> findByToken(String token);
